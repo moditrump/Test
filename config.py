5be24bafc03c1541a4834685a47fb47db1504b01
krishnarjun00 <156284133+krishnarjun00@users.noreply.github.com>
2024-06-08 11:46:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6389558385:AAESGI-Wb5U9ErEwYh0zI7gQF0R6rKKwEc4")
+    API_ID = int(os.environ["API_ID", 22308019]
+    API_HASH = os.environ["API_HASH", "fe49bff9eaadccecb58ded07aff33e88"]
+    AUTH_USERS = "7079041090"""
+
