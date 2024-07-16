a68ab33b26116c4af93f28cbcc773e6bc536a2b8
sakshi24687 <goverd097@gmail.com>
2024-07-16 22:49:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6744315420:AAEF5llbGO2Rf6JjcGP0Pzy61I7Z693-AL8")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7339200833:AAHenSRgtWTItqqz6euvfYQKsHsmNy6LiXI")
     API_ID = int(os.environ["API_ID", 22308019]
     API_HASH = os.environ["API_HASH", "fe49bff9eaadccecb58ded07aff33e88"]
     AUTH_USERS = "5829511291"""
